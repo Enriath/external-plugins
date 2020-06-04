04dfc0e0974b28080599bb1ffdc3f7195df922d6
Hydrox6 <ikada@protonmail.ch>
2020-06-04 12:18:35 +0100

    1.0

---
+++ src/main/java/io/hydrox/doorkicker/DoorKickerPlugin.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2020, Hydrox6 <ikada@protonmail.ch>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package io.hydrox.doorkicker;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.Perspective;
+import net.runelite.api.Player;
+import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.ClientTick;
+import net.runelite.api.events.MenuOptionClicked;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import javax.inject.Inject;
+
+@PluginDescriptor(
+	name = "Door Kicker",
+	description = "Assert your dominance against the doors"
+)
+@Slf4j
+public class DoorKickerPlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
+
+	private LocalPoint doorLocation;
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked e)
+	{
+		if (e.getMenuOption().equals("Open") && e.getMenuTarget().endsWith("Door"))
+		{
+			clientThread.invokeLater(() -> doorLocation = client.getLocalDestinationLocation());
+		}
+		else
+		{
+			clientThread.invokeLater(() -> doorLocation = null);
+		}
+	}
+
+	@Subscribe
+	public void onClientTick(ClientTick e)
+	{
+		if (doorLocation != null)
+		{
+			Player local = client.getLocalPlayer();
+			if (doorLocation.distanceTo(local.getLocalLocation()) <= Perspective.LOCAL_TILE_SIZE)
+			{
+				local.setAnimation(423);
+				local.setActionFrame(0);
+				doorLocation = null;
+			}
+		}
+	}
+}
