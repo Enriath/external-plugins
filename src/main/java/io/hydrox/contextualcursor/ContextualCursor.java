7f5cfd6421ec0834aecc66b172724a299739f5c5
Enriath <ikada@protonmail.ch>
2022-08-10 11:22:56 +0100

    author update

--- src/main/java/io/hydrox/contextualcursor/ContextualCursor.java
+++ src/main/java/io/hydrox/contextualcursor/ContextualCursor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Hydrox6 <ikada@protonmail.ch>
+ * Copyright (c) 2019-2022 Enriath <ikada@protonmail.ch>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
